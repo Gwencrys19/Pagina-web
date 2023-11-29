5580d9655356e8d0a372cf0d3c1ea73f45463794
Gwencrys19 <--unset>
2023-11-29 17:19:55 -0500

    prueba

---
+++ html2pdf.js
@@ -0,0 +1,4 @@
+function descargarPDF() {
+    var elemento = document.getElementById('prueba');  // Reemplaza 'contenido' con el ID de la sección que deseas convertir a PDF
+    html2pdf(elemento);
+}
